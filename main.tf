8e66efaf746fec9eb0fa1642f422d5350530fb8a
TimBennett13 <108099558+TimBennett13@users.noreply.github.com>
2025-07-29 14:10:22 -0400

    edit puncuation

--- main.tf
+++ main.tf
@@ -59,7 +59,7 @@ resource "aws_security_group_rule" "blog_all_out" {
   type = "ingress"
   from_port = 0
   to_port = 0
-  protocol  =  "-1
+  protocol  =  "-1"
   cidr_blocks = ["0.0.0.0/0"]
 
   security_group_id = aws_security_group.blog.id
